98acaddee21490d3f7f421349a3c4143e02d9482
PLucianP <paullucianp@gmail.com>
2025-01-16 10:38:17 +0200

    Initial commit for SCCM

--- tssd/urls.py
+++
@@ -1,9 +0,0 @@
-# urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home_page, name='home'),
-    path('api/upload/', views.upload_media, name='upload_media'),
-    path('api/process-live/', views.process_live_frame, name='process_live_frame'),
-]
